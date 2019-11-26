fb857d54e84777d7ae5fec10779f721b2d3a3f33
diyoush <diybald@yahoo.fr>
2019-11-26 16:58:18 +0100

    Update Goal_Think.cpp

--- Buckland_Chapter7 to 10_Raven/goals/Goal_Think.cpp
+++ Buckland_Chapter7 to 10_Raven/goals/Goal_Think.cpp
@@ -33,7 +33,7 @@ Goal_Think::Goal_Think(Raven_Bot* pBot):Goal_Composite<Raven_Bot>(pBot, goal_thi
   double RailgunBias = RandInRange(LowRangeOfBias, HighRangeOfBias);
   double ExploreBias = RandInRange(LowRangeOfBias, HighRangeOfBias);
   double AttackBias = RandInRange(LowRangeOfBias, HighRangeOfBias);
-  double GrenadeBias = 1.5;
+	
 
   //create the evaluator objects
   m_Evaluators.push_back(new GetHealthGoal_Evaluator(HealthBias));
@@ -45,8 +45,6 @@ Goal_Think::Goal_Think(Raven_Bot* pBot):Goal_Composite<Raven_Bot>(pBot, goal_thi
                                                      type_rail_gun));
   m_Evaluators.push_back(new GetWeaponGoal_Evaluator(RocketLauncherBias,
                                                      type_rocket_launcher));
-  m_Evaluators.push_back(new GetWeaponGoal_Evaluator(GrenadeBias,
-	  type_grenade));
 }
 
 //----------------------------- dtor ------------------------------------------
